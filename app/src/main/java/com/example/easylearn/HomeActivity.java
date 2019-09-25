7cbfc5e80d6ba8c9b751328338de7f12c139ffd7
devansh071999 <devanshsampatjain@gmail.com>
2019-09-25 16:21:27 +0530

    First commit

---
+++ app/src/main/java/com/example/easylearn/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.easylearn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+Button btnLogout;
+FirebaseAuth mFirebaseAuth;
+private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+        btnLogout=findViewById(R.id.logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this,MainActivity.class));
+            }
+        });
+
+
+
+    }
+}
